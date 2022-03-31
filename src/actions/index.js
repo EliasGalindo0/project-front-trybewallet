d87d5ab7fe2a9c5aab819234e9029ae25161537d
EliasGalindo0 <eliasgalindo@gmail.com>
2022-03-31 16:11:12 -0300

    Set up Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
-// Coloque aqui suas actions
+// ---ACTIONS---
+// Adicionar, remover e editar um gasto;
+// Visualizar uma tabela com seus gastos;
+// Visualizar o total de gastos convertidos para uma moeda de escolha;
+// chaves "user" e "wallet" no seu estado global
+
+const newAction = (state) => ({ type: 'NEW_ACTION', state });
+
+export default newAction;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,32 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+//  Initial State
+// {
+//   user: {
+//     email: '',
+//   },
+//   wallet: {
+//     currencies: [],
+//     expenses: []
+//   }
+// }
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'NEW_ACTION':
+    return { state: action.state };
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
